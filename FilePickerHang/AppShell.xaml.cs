bf79e56d6de22bfc82c8f47c0d48c3005cd6e0f8
Elizabeth Schibig <ElizabethS@digital-control.com>
2025-06-27 10:18:49 -0700

    initial commit

---
+++ FilePickerHang/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FilePickerHang
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FilePickerHang/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace FilePickerHang
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private readonly List<Task<FileResult?>> _tasks = [];
+
+        private async void CounterBtn_Clicked(object sender, EventArgs e)
+        {
+            var completedTasks = _tasks.Where(t => t.IsCompleted).Count();
+            var inProgressTasks = _tasks.Where(t => !t.IsCompleted).Count();
+            TaskCountLabel.Text = $"Picked tasks: {completedTasks}, Hanging tasks: {inProgressTasks}";
+
+            var fileResultTask = FilePicker.PickAsync(new PickOptions
+            {
+                PickerTitle = "Please select a file"
+            });
+
+            _tasks.Add(fileResultTask);
+            var fileResult = await fileResultTask;
+
+            if (fileResult == null)
+            {
+                return;
+            }
+
+            FileNameLabel.Text = fileResult.FileName;
+        }
+    }
+}
